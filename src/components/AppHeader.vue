280857febd30922cb91a95d09048411da135e0d2
reistence <gvlamberti@gmail.com>
2023-02-10 18:28:42 +0100

    header draft

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,220 @@
+<script>
+// import { onMounted } from "vue";
+import HeaderNav from "./HeaderNav.vue";
+export default {
+  name: "AppHeader",
+  components: {
+    HeaderNav,
+  },
+  data() {
+    return {
+      navHeaderList: [
+        {
+          name: "home",
+          link: "/home",
+        },
+        {
+          name: "pages",
+          link: "/pages",
+        },
+        {
+          name: "program",
+          link: "/programs",
+        },
+        {
+          name: "tickets",
+          link: "/tickets",
+        },
+        {
+          name: "speakers",
+          link: "/speakers",
+        },
+        {
+          name: "papers",
+          link: "/papers",
+        },
+        {
+          name: "blog",
+          link: "/blog",
+        },
+        {
+          name: "shortcodes",
+          link: "/shortcodes",
+        },
+      ],
+      scrollPosition: null,
+      hamMenu: false,
+    };
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+    showMenu() {
+      this.hamMenu = !this.hamMenu;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+    console.log(1);
+  },
+};
+</script>
+<template>
+  <header :class="{ faded: scrollPosition > 50 }">
+    <div class="container">
+      <div class="logo">
+        <img src="../assets/logo.png" alt="Boolking" />
+      </div>
+
+      <nav>
+        <ul>
+          <HeaderNav
+            v-for="(item, index) in navHeaderList"
+            :key="index"
+            :NavItems="item"
+          >
+          </HeaderNav>
+          |
+          <li class="lens">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </li>
+        </ul>
+      </nav>
+      <div class="ham-nav">
+        <i @click="showMenu" class="fa-solid fa-bars"></i>
+        <ul v-show="hamMenu">
+          <HeaderNav
+            v-for="(item, index) in navHeaderList"
+            :key="index"
+            :NavItems="item"
+          ></HeaderNav>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+header {
+  position: fixed;
+  height: 70px;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  background-color: $white;
+  width: 100%;
+
+  &.faded {
+    background-color: rgba(255, 255, 255, 0.594);
+    transition: 500ms;
+    backdrop-filter: blur(10px);
+    height: 40px;
+    box-shadow: 1px 1px 5px rgb(18, 1, 1);
+
+    &:hover {
+      background-color: $white;
+      filter: blur(0);
+      height: 70px;
+    }
+
+    .container {
+      justify-content: space-between;
+      padding: 0;
+    }
+  }
+  .container {
+    padding: 1em 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+
+    .logo {
+      width: 2em;
+      transition: 500ms;
+      cursor: pointer;
+      &:hover {
+        transform: scale(1.1);
+        transition: 500ms;
+      }
+      img {
+        max-width: 100%;
+        object-fit: contain;
+      }
+    }
+
+    nav {
+      width: 70%;
+    }
+
+    ul {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      justify-content: space-between;
+      align-items: center;
+      gap: 1em;
+      color: $gray;
+
+      li {
+        color: black;
+        position: relative;
+        cursor: pointer;
+      }
+      i {
+        font-size: 1rem;
+        color: $gray;
+        transition: 300ms;
+
+        &:hover {
+          transform: scale(1.1);
+          transition: 300ms;
+        }
+      }
+    }
+  }
+
+  .ham-nav {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 800px) {
+  header {
+    .container {
+      nav {
+        display: none;
+      }
+
+      .ham-nav {
+        display: block;
+        position: relative;
+        cursor: pointer;
+        background-color: inherit;
+
+        ul {
+          position: absolute;
+          top: 40px;
+          left: -70px;
+          display: flex;
+          flex-direction: column;
+          background-color: rgba(255, 255, 255, 0.594);
+          padding: 1em 0.5em;
+          align-items: flex-start;
+          border-radius: 5px;
+          transition: 500ms;
+          backdrop-filter: blur(10px);
+
+          &:hover {
+            background-color: $white;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,63 @@
+<script>
+
+export default {
+  name: "HeaderNav",
+  props: {
+    NavItems: Object,
+  },
+};
+</script>
+
+<template>
+  <li>
+    <a :href="NavItems.link">{{ NavItems.name }}</a>
+  </li>
+</template>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+li {
+//   text-transform: uppercase;
+  color: $red;
+  font-weight: light;
+  font-size: 0.7rem;
+  position: relative;
+
+  &:hover {
+    color: $red;
+    transition: 200ms;
+  }
+
+  &:after {
+    content: " ";
+    background-color: $red;
+    width: 100%;
+    position: absolute;
+    height: 0px;
+    bottom: -10px;
+    left: 0;
+    z-index: 999;
+    display: inline-block;
+  }
+
+  &:hover::after {
+    animation: grow-border 200ms;
+    height: 3px;
+  }
+
+  @keyframes grow-border {
+    0% {
+      height: 0;
+    }
+    100% {
+      height: 2px;
+    }
+  }
+}
+
+// @media screen and (max-width: 850px) {
+//   li {
+//     display: none;
+//   }
+// }
+</style>
