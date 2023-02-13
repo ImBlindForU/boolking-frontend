88633974c9fd0eccb67711d375102804f0a0884a
reistence <gvlamberti@gmail.com>
2023-02-13 11:49:13 +0100

    estates card

---
+++ src/components/EstateCard.vue
@@ -0,0 +1,110 @@
+<script>
+// import axios from "axios";
+import { store } from "../store";
+
+export default {
+  name: "EstateCard",
+  props: {
+    estate: Object,
+  },
+  data() {
+    return {
+      store
+    };
+  },
+  computed: {
+  },
+  methods: {   
+   
+  },
+  mounted(){
+
+    gsap.fromTo(".estate-card", {
+      opacity: 0,
+      scale: 0.5,
+      duration: .5, 
+      stagger: .1
+
+    },{
+      duration: .5,
+      opacity: 1,
+      scale: 1,
+      y: 0,
+      stagger: .1
+    })
+
+  }
+};
+</script>
+
+<template>
+ <div  v-if="estate.is_visible" class="estate-card ">
+        <div class="estate-img">
+            <img :src="`http://127.0.0.1:8000/storage/${estate.cover_img}`" alt="" srcset="">
+        </div>
+        <div class="estate-txt">
+            <div>
+                <h5>{{ estate.title }}</h5>
+                <p>Tipologia: {{ estate.type }}</p>
+                <p>&#x33A1;: {{ estate.mq }}</p>
+                <p>Prezzo: {{ estate.price }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+ .estate-card{
+           border: 1px solid #c1baba;
+           border-radius: 15px;
+           overflow: hidden;
+           width: calc(100% / 1 - .25em) !important;
+           display: flex;
+           flex-direction: column;
+           // justify-content: center;
+           // align-items: center;
+           .estate-img{
+               width: 100%;
+               img{
+               width: 100%;
+                   max-width: 100%;
+                   object-fit: cover;
+               }
+           }
+   
+           .estate-txt
+           {
+               background-color: #fff;
+               padding: .5em;
+               display: flex;
+               direction: row;
+               justify-content: space-between;
+   
+               p{
+                   margin-bottom: .1em !important;
+               }
+           }
+       
+           transition: all 300ms;
+           &:hover{
+               transition: all 300ms;
+               transform: scale(1.1);
+               z-index: 9;
+               border-bottom: 3px solid #ff5a60;
+               box-shadow: 0px 10px 10px #827b7b;
+               z-index: 99;
+              
+              
+           }
+           @media screen and (min-width: 500px) {
+               width: calc(100% / 2 - .5em) !important;
+               }
+           @media screen and (min-width: 800px) {
+               width: calc(100% / 3 - .5em) !important;
+               }
+           @media screen and (min-width: 1200px) {
+               width: calc(100% / 4 - .5em) !important;
+               }
+       }
+
+</style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -2,9 +2,10 @@
 import axios from "axios";
 import {store} from "../store"
 import AppWelcomeAnimation from"../components/AppWelcomeAnimation.vue"
+import EstateCard from "../components/EstateCard.vue"
 export default {
   name: "AppMainContent",
-  components: { AppWelcomeAnimation },
+  components: { AppWelcomeAnimation, EstateCard },
   data() {
     return {
       // BackEndUrl: "http://127.0.0.1:8000/api/estates",
@@ -38,42 +39,10 @@ export default {
     <h1>Tutti i nostri alloggi</h1>
 
     <div class="container">
-
-      <!-- {{-- OPZIONE CARD --}} -->
                 <div class="cards-container ">
-
-                   
-                        <div v-for="estate in store.allEstates" :key="estate.id" class="estate-card ">
-                            <div class="estate-img">
-                              
-                                <img :src="`http://127.0.0.1:8000/storage/${estate.cover_img}`" alt="" srcset="">
-                            </div>
-                            <div class="estate-txt">
-                                <div>
-                                    <h5>{{ estate.title }}</h5>
-                                    <p>Tipologia: {{ estate.type }}</p>
-                                    <p>&#x33A1;: {{ estate.mq }}</p>
-                                    <p>Prezzo: {{ estate.price }}</p>
-                                </div>
-                                <div class="estate-actions">
-                                    <!-- <a class="btn our-btn d-block mb-1 "
-                                        href="{{ route('user.estates.show', $estate->slug) }}">
-                                      </a> -->
-                                      <i class="fa-solid fa-magnifying-glass"></i>
-                                    <!-- <a class="btn our-btn d-block  mb-1 "
-                                        href="{{ route('user.estates.edit', $estate->slug) }}">
-                                      </a> -->
-                                      <i class="fa-solid fa-wrench"></i>
-                                </div>
-                            </div>
-
-
-                        </div>
+                    <EstateCard v-for="estate in store.allEstates"
+                     :estate="estate" ></EstateCard>
                 </div>
-<!-- 
-                {{-- OPZIONE CARD --}} -->
-
-
     </div>
 
  
@@ -87,7 +56,6 @@ export default {
   @include my-container-fluid;
   padding-top: 1em;
   color: black;
-  // background: #000;
   height: 100%;
 
   h1 {
@@ -95,15 +63,13 @@ export default {
     text-align: center;
   }
 
-  .wrapper {
-    @include my-flex(column, space-between);
+  .container {
+    @include my-container;
     align-items: center;
-    border: 1px solid gray;
     gap: 1.5em;
-    margin: 2em 0;
+    margin-top: 2em;
     padding: 1em 0 4em;
     height: 100%;
-    background-color: black;
   }
 
 
@@ -112,58 +78,7 @@ export default {
     @include my-flex(row, center);
     flex-wrap: wrap;
    gap: 1em .5em;
-       .estate-card{
-           border: 1px solid #c1baba;
-           border-radius: 15px;
-           overflow: hidden;
-           width: calc(100% / 1 - .25em) !important;
-           display: flex;
-           flex-direction: column;
-           // justify-content: center;
-           // align-items: center;
-           .estate-img{
-               width: 100%;
-               img{
-               width: 100%;
-                   max-width: 100%;
-                   object-fit: cover;
-               }
-           }
-   
-           .estate-txt
-           {
-               background-color: #fff;
-               padding: .5em;
-               display: flex;
-               direction: row;
-               justify-content: space-between;
-   
-               p{
-                   margin-bottom: .1em !important;
-               }
-           }
-       
-           transition: all 300ms;
-           &:hover{
-               transition: all 300ms;
-               transform: scale(1.1);
-               z-index: 9;
-               border-bottom: 3px solid #ff5a60;
-               box-shadow: 0px 10px 10px #827b7b;
-               z-index: 99;
-              
-              
-           }
-           @media screen and (min-width: 500px) {
-               width: calc(100% / 2 - .5em) !important;
-               }
-           @media screen and (min-width: 800px) {
-               width: calc(100% / 3 - .5em) !important;
-               }
-           @media screen and (min-width: 1200px) {
-               width: calc(100% / 4 - .5em) !important;
-               }
-       }
+      
        
    
    }
