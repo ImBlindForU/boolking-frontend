544c397516d507e4235064ccf955111ead712393
reistence <gvlamberti@gmail.com>
2023-02-14 11:18:29 +0100

    card animation fixed

--- src/components/EstateCard.vue
+++ src/components/EstateCard.vue
@@ -22,17 +22,20 @@ export default {
   mounted(){
 
     gsap.fromTo(".estate-card", {
+      // x: "-800%",
       opacity: 0,
-      scale: 0.5,
-      duration: .5, 
-      stagger: .1
+      alpha: 0,
+      scale: 0.1,
+      duration: .2, 
+      stagger: .2
 
     },{
       duration: .5,
       opacity: 1,
       scale: 1,
-      y: 0,
-      stagger: .1
+      duration: .2, 
+      // x: 0,
+      stagger: .2
     })
 
   }
