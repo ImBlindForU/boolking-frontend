d88f49e95ad1ae641e95a68effb7ca091a57a25d
reistence <gvlamberti@gmail.com>
2023-02-15 18:52:53 +0100

    draft show layout

--- src/pages/EstatePage.vue
+++ src/pages/EstatePage.vue
@@ -2,6 +2,8 @@
 <script>
 import axios from 'axios';
 import { store } from '../store';
+import tt from '@tomtom-international/web-sdk-maps';
+
 
 export default {
   name: "EstatePage",
@@ -9,6 +11,9 @@ export default {
     return {
       store,
       estate: {},
+      map: null,
+      KEY: "e3ENGW4vH2FBakpfksCRV16OTNwyZh0e",
+
     };
   },
   created() {
@@ -24,8 +29,71 @@ export default {
         }else {
           this.$router.push({name:'Not-Found'})
         }
+
+        this.initializeMap();
+
       })
-    }
+    },
+    initializeMap() {
+            // this.$refs.mapRef.innerHTML = ""
+            let center = [];
+
+              if(this.estate){
+                console.log(this.estate);
+                center = [this.estate.address.long, this.estate.address.lat]
+              } else {
+                center = [12.4964, 41.9028]
+              }
+
+              const map = tt.map({
+                key: this.KEY,
+                container: this.$refs.mapRef,
+                center: center,
+                zoom: 9
+              })
+
+
+              // this.allEstates.forEach(estate => {
+               const markerElement = document.createElement("div")
+                markerElement.id = "marker"
+                const logo = new URL("../assets/marker.png", import.meta.url).href
+                markerElement.style.backgroundImage = `url(${logo})`
+                markerElement.style.width = "30px"
+                markerElement.style.padding = ".5em"
+
+                markerElement.style.height = "30px"
+                markerElement.style.backgroundSize = "cover"
+                markerElement.style.borderRadius= "20px"
+               const marker = new tt.Marker({ element: markerElement, anchor: "center"})
+                   .setLngLat([this.estate.address.long, this.estate.address.lat])
+                   .addTo(map);
+                   const popupOffsets = {
+                         top: [0, 0],
+                         bottom: [0, -15],
+                         "bottom-right": [0, -70],
+                         "bottom-left": [0, -70],
+                         left: [25, -35],
+                         right: [-25, -35],
+                       }
+
+
+                   //custom popup
+                   const customPopUp = document.createElement("div")
+                   customPopUp.id = "my-pop-up"
+                   customPopUp.innerHTML = `<p> ${this.estate.title} </br> ${this.estate.price ? this.estate.price : "Non specificato"} </p>`
+                   customPopUp.style.color = "black"
+                   customPopUp.style.width = "150px"
+
+                   // const popup = new tt.Popup({ offset: popupOffsets, closeOnMove: true }).setDOMContent(customPopUp)
+                   const popup = new tt.Popup({offset: popupOffsets, closeOnMove: true}).setDOMContent(customPopUp)
+
+                 marker.setPopup(popup)
+            //  });
+
+
+
+
+        },
     },
 };
 </script>
@@ -37,17 +105,26 @@ export default {
         </div>
         <div class="container-text">
             <div class="estate-show-txt">
-              <div>
+             
                   <h5>{{ estate.title }}</h5>
-                  <p>Tipologia: {{ estate.type }}</p>
-                  <p>&#x33A1;: {{ estate.mq }}</p>
-                  <p>Prezzo: {{ estate.price }}</p>
-              </div>
+                  <p> <span>Tipologia:</span> {{ estate.type }}</p>
+                  <p> <span>&#x33A1;:</span> {{ estate.mq }}</p>
+                  <p><span>Prezzo:</span>  {{ estate.price }}</p>
+                  <p><span>Descrizione:</span>  {{ estate.description }}</p>
+                  <p><span>Servizi:</span></p>
+                  <ul id="services">
+                    <li v-for="service in estate.services">{{ service.name }}</li>
+                  </ul>
+             
           </div>
-          <div class="message-show-box">
-            <input placeholder="Inserisci la tua mail" type="text">
-            <textarea placeholder="Inserisci le tue domande" name="" id="" cols="30" rows="10"></textarea>
-            <a class="our-btn" href="">Invia Email</a>
+          <div class="additional">
+            <div id='tom-map' ref="mapRef"></div>
+
+            <div class="message-show-box">
+              <input placeholder="Inserisci la tua mail" type="text">
+              <textarea placeholder="Inserisci le tue domande" name="" id="" cols="30" rows="10"></textarea>
+              <a class="our-btn" href="">Invia Email</a>
+            </div>
           </div>
         </div>
         
@@ -58,18 +135,66 @@ export default {
 @use "../styles/partials/_mixins" as *;
 @use "../styles/partials/_variables" as *;
 
+
+
 .estate-show-img{
   display: flex;
   padding-top: 3rem;
 }
 .estate-show-txt{
   padding-top: 2rem;
+  width: 50%;
+
+  p{
+    font-weight: 200;
+     
+    span{
+      font-weight: 500;
+      color: $red;
+    }
+  }
+
+  #services{
+      margin: .3em .5em;
+      @include my-flex(column, flex-start);
+      height: 200px;
+      flex-wrap: wrap;
+      align-items: flex-start;
+      @media screen and (max-width: 500px) {
+                  flex-wrap: nowrap;
+                 }
+      li{
+        margin-left: .5em !important;
+      }
+    
+  }
 }
 
 .container-text{
+  margin-top: 2em;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+
+
+  @media screen and (max-width: 500px) {
+                  flex-wrap: wrap;
+                  }
 }
+
+.additional{
+  @include my-flex(column, center);
+  width: 50%;
+  @media screen and (max-width: 500px) {
+                  width: 100%;
+                  }
+#tom-map {
+      margin: 0;
+      height: 17em;
+      width: 100%;
+      border-radius: 20px;
+      box-shadow: 0px 5px 5px #827b7b;
+    
+    }
 .message-show-box{
   padding-top: 3rem;
   display: flex;
@@ -82,5 +207,6 @@ export default {
     text-align: center;
     border: 1px solid red;
   }
+}
 }
 </style>
