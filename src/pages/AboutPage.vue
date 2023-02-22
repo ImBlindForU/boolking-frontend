06fb45198171504b076ac6bd627a156bc4097c9e
reistence <gvlamberti@gmail.com>
2023-02-22 12:45:24 +0100

    query 4 about

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,11 +1,87 @@
 <script>
 export default {
-    
+    name: "AboutPage",
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        getImgPath(imgName, format){
+            return new URL(`../assets/us/${imgName}.${format}`, import.meta.url).href
+        }
+    },
+    mounted(){
+        const wrapper = document.getElementById("wrapper");
+
+                const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+                const uniqueRand = (min, max, prev) => {
+                let next = prev;
+
+                while (prev === next) next = rand(min, max);
+
+                return next;
+                };
+
+                const combinations = [
+                { configuration: 1, roundness: 1 },
+                { configuration: 1, roundness: 2 },
+                { configuration: 1, roundness: 4 },
+                { configuration: 3, roundness: 2 },
+                { configuration: 3, roundness: 3 },
+                { configuration: 3, roundness: 3 },
+                { configuration: 2, roundness: 2 },
+                { configuration: 2, roundness: 3 },
+                { configuration: 2, roundness: 4 },
+                { configuration: 2, roundness: 1 },
+                ];
+
+                let prev = 0;
+
+                setInterval(() => {
+                const index = uniqueRand(0, combinations.length - 1, prev),
+                    combination = combinations[index];
+
+                wrapper.dataset.configuration = combination.configuration;
+                wrapper.dataset.roundness = combination.roundness;
+
+                prev = index;
+                }, 2000);
+
+                // wrapper.dataset.configuration =2;
+                // wrapper.dataset.roundness = 4;
+    }
 }
 </script>
 <template>
     <div class="container">
         <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima harum architecto et animi omnis praesentium quis tenetur quasi facere labore earum impedit saepe, doloribus, reiciendis accusamus incidunt dolorem! Ut tempore minus adipisci natus dicta vitae rem ratione! Provident facere facilis itaque delectus sed illum, cupiditate perspiciatis! Eos tempore recusandae esse perferendis aut expedita incidunt autem molestiae alias possimus tempora itaque ipsum unde minus reprehenderit, repellat voluptate, quas ex voluptas commodi similique rem, eum sit aspernatur. Id accusamus quae vero, ducimus totam dicta! <br> I responsabili sono:</p>
+        <div id="wrapper" data-configuration="1" data-roundness="5"> 
+      <div class="shape"><div class="img">
+        <a href="https://github.com/BetterCallAle">
+            <img :src="getImgPath('alessio', 'png')" alt="" srcset="">
+        </a>
+      </div> </div>
+      <div class="shape"><div class="img">
+        <a href="https://github.com/FabrizioMisseri">
+            <img :src="getImgPath('fabrizio', 'jpg')" alt="" srcset="">
+        </a>
+      </div></div>
+      <div class="shape"><div class="img">
+        <a href="https://github.com/ImBlindForU">
+            <img :src="getImgPath('samuele', 'jpeg')" alt="" srcset="">
+        </a>
+      </div></div>
+      <div class="shape"><div class="img">
+        <a href="https://github.com/reistence">
+            <img :src="getImgPath('valerio', 'jpeg')" alt="" srcset="">
+        </a>
+
+      </div></div>
+     
+    </div>
 
     </div>
 </template>
@@ -15,8 +91,260 @@ export default {
 @use "../styles/partials/_variables" as *;
 
 .container{
-    @include my-container;
+  @include my-container;
+  height: 80vh;
+//   width: 100vw;
+  display: grid;
+  place-items: center;
+  margin: 0px auto 2em;
+//   overflow: hidden;
 }
+
+
+#wrapper {
+  aspect-ratio: 1.618;
+  width: 90vmin;
+  position: relative;
+  margin-top: 1em;
+}
+
+#wrapper > .shape {
+  height: 30%;
+  width: 30%;
+  margin: 0 auto;
+  background-color: rgb(239, 78, 78);
+  position: absolute;
+  transition: left, top, height, width, border-radius;
+  transition-duration: 750ms;
+  transition-timing-function: ease-in-out;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
+}
+
+
+.shape > div{
+//   background-color: black;
+  width: 5em;
+  height: 5em;
+  border-radius: 50%;
+
+   a > img{
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+
+
+#wrapper > .shape:nth-child(1) {
+  background-color: rgb(239, 78, 78);
+  z-index: 2;
+}
+
+#wrapper > .shape:nth-child(2) {
+  background-color: rgb(239, 78, 78);
+  z-index: 2;
+}
+
+#wrapper > .shape:nth-child(3) {
+  background-color: rgb(239, 78, 78);
+  z-index: 1;
+}
+
+#wrapper > .shape:nth-child(4) {
+  background-color: rgb(239, 78, 78);
+  z-index: 2;
+}
+
+#wrapper > .shape:nth-child(5) {
+  background-color: rgb(239, 78, 78);
+  z-index: 2;
+}
+
+#wrapper > .shape:nth-child(6) {
+  background-color: rgb(239, 78, 78);
+  z-index: 2;
+}
+
+#wrapper > .shape:nth-child(7) {
+  background-color: rgb(239, 78, 78);
+  z-index: 2;
+}
+
+#wrapper[data-configuration="1"] > .shape:nth-child(1) {
+  left: 0%;
+  top: 0%;
+  height: 50%;
+  width: 40%;
+}
+
+#wrapper[data-configuration="1"] > .shape:nth-child(2) {
+  left: 40%;
+  top: 0%;
+  height: 50%;
+  width: 40%;
+}
+
+#wrapper[data-configuration="1"] > .shape:nth-child(3) {
+  left: 50%;
+  top: 50%;
+  height: 50%;
+  width: 40%;
+}
+
+#wrapper[data-configuration="1"] > .shape:nth-child(4) {
+  left: 10%;
+  top: 50%;
+  height: 50%;
+  width: 40%;
+}
+
+
+
+#wrapper[data-configuration="3"] > .shape:nth-child(1) {
+  left: 0%;
+  top: 55%;
+  height: 50%;
+  width: 50%;
+}
+
+#wrapper[data-configuration="3"] > .shape:nth-child(2) {
+  left: -5%;
+  top: 2.7%;
+  height: 55%;
+  width: 44%;
+}
+
+#wrapper[data-configuration="3"] > .shape:nth-child(3) {
+  left: 38%;
+  top: 5%;
+  height: 50%;
+  width: 50%;
+}
+
+#wrapper[data-configuration="3"] > .shape:nth-child(4) {
+  left: 50%;
+  top: 55%;
+  height: 55%;
+  width: 44%;
+}
+#wrapper[data-configuration="2"] > .shape:nth-child(1) {
+  left: 0%;
+  top: 0%;
+  height: 25%;
+  width: 100%;
+  margin: 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1em;
+
+  @media screen and (max-width: 516px ) {
+        
+    & > .img {
+        width: 3em;
+        height: 3em;
+    }
+  }
+}
+
+#wrapper[data-configuration="2"] > .shape:nth-child(2) {
+  left: 0%;
+  top: 25%;
+  height: 25%;
+  width: 100%;
+  margin: 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1em;
+
+  @media screen and (max-width: 516px ) {
+        
+        & > .img {
+            width: 3em;
+            height: 3em;
+        }
+      }
+}
+
+#wrapper[data-configuration="2"] > .shape:nth-child(3) {
+  left: 0%;
+  top: 50%;
+  height: 25%;
+  width: 100%;
+  margin: 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1em;
+
+  @media screen and (max-width: 516px ) {
+        
+        & > .img {
+            width: 3em;
+            height: 3em;
+        }
+      }
+}
+
+#wrapper[data-configuration="2"] > .shape:nth-child(4) {
+  left: 0%;
+  top: 75%;
+  height: 25%;
+  width: 100%;
+  margin: 0 auto;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1em;
+  @media screen and (max-width: 516px ) {
+        
+        & > .img {
+            width: 3em;
+            height: 3em;
+        }
+      }
+}
+
+
+
+#wrapper[data-roundness="1"] > .shape {
+  border-radius: 6rem;
+}
+
+#wrapper[data-roundness="2"] > .shape {
+  border-radius: 0rem;
+}
+
+#wrapper[data-roundness="3"] > .shape {
+  border-radius: 30rem;
+}
+
+#wrapper[data-roundness="4"] > .shape:nth-child(1) {
+  border-bottom-left-radius: 10rem;
+}
+
+#wrapper[data-roundness="4"] > .shape:nth-child(2) {
+  /* border-radius: 20rem; */
+  border-top-left-radius: 10rem;
+
+}
+
+#wrapper[data-roundness="4"] > .shape:nth-child(3) {
+  border-top-right-radius: 12rem;
+}
+
+#wrapper[data-roundness="4"] > .shape:nth-child(4) {
+  /* border-top-right-radius: 10rem; */
+  border-bottom-right-radius: 10rem;
+}
+
+
+
+
 
 
 
